785862018f43ebf33f2005c5525ff56fcadacd38
djharish <djharish@itcom888.com>
2024-04-06 18:54:32 +0800

    1

--- src/view/CellMall.vue
+++ src/view/CellMall.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        积分商城
+        <img src="https://img0.baidu.com/it/u=775932288,286321380&fm=253&fmt=auto&app=138&f=PNG?w=767&h=393" style="width: 100%;height: 140px;">
+        
     </div>
 </template>
 <script>
